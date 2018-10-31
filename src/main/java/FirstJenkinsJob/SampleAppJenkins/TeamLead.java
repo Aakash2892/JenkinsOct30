26847f0f50525d7d57b19a3ba30a7c015eb51101
srinivasubraunswick <srinuchinmaya@gmail.com>
2018-10-31 10:26:58 +0530

    Javafiles123

---
+++ src/main/java/FirstJenkinsJob/SampleAppJenkins/TeamLead.java
@@ -0,0 +1,13 @@
+package FirstJenkinsJob.SampleAppJenkins;
+
+
+public class TeamLead extends Employee{
+	private int numberOfProjectsManaged;
+
+	public int getNumberOfProjectsManaged() {
+		return numberOfProjectsManaged;
+	}
+	public void setNumberOfProjectsManaged(int numberOfProjectsManaged) {
+		this.numberOfProjectsManaged = numberOfProjectsManaged;
+	}	
+}
